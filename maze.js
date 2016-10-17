60487eab9bfd43e3d820d59c27e36c75fa045644
JayWall <sanjah.hall@gmail.com>
2016-10-16 20:05:55 -0500

    The Beginning

--- maze.js
+++ maze.js
@@ -1,24 +1,22 @@
+var gameLost = false;
+var start = false;
+var instruct = "";
 
-document.getElementById("boundary1").onmouseover = testerFunction;
+function turnRed() {
+    var walls = document.getElementsByClassName("boundary");
+    for (var i=0; i < walls.length; i++) {
+        walls[i].setAttribute("class", "boundary youlose");
+    }
 
-function testerFunction(){
-	var x = document.getElementByClassName("boundary");
-	for (var i=0; i < x.length; i++){
-		x[i].style.backgroundColor = "red";
-	}
-
-}
-/*
-document.getElementById("boundary1").onmouseover = function() {mouseOver()};
-document.getElementById("boundary1").onmouseout = function() {mouseOut()};
-
-function mouseOver() {
-    document.getElementById("boundary1").style.color = "red";
+    gameLost = true;
+    document.getElementById("status").textContent = "Game Over... You Lose";
 }
 
-function mouseOut() {
-    document.getElementById("boundary1").style.color = "black";
-}*.
 
 
 
+window.onload = function() {
+    instruct = document.getElementById("status").textContent;
+    checkWalls();
+    
+}
